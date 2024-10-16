ac62f261514708ca88ca74602e9cc479733f665a
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-10-16 12:26:48 +0700

    Module9: complete

--- Module9/Program.cs
+++ Module9/Program.cs
@@ -1,11 +1,57 @@
-﻿namespace Module9
+﻿using System;
+
+namespace Module9_1;
+
+public class CustomException : Exception // собственное исключание
 {
-    internal class Program
+    public CustomException(string message) : base(message) { } // без реализации, потому что я всё же плохо понимаю, какие ещё нестандартные исключаеня могу предусмотреть
+}
+
+internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Exception[] excpts = new Exception[]
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            new DivideByZeroException(),
+            new ArgumentNullException("Argument is null"),
+            new IndexOutOfRangeException(),
+            new CustomException("This is a custom exception"),
+            new InvalidOperationException()
+        };
+
+        foreach (var expt in excpts)
+        {
+            try
+            {
+                throw expt;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Seems like you try to divide by zero");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Аrgument is null: {ex}");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Out of bounds");
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine($"Custom exception: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Неизвестное исключение: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally, some good f**king food");
+            }
         }
+
     }
+
 }
---
+++ Module9_2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Module9_2;
+class Program
+{
+    public delegate void SortEventHandler(int sortOrder);
+    public event SortEventHandler SortEvent;
+
+    private List<string> surnames = new List<string> { "Иванов", "Петров", "Сидоров", "Смирнов", "Кузнецов" };
+
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.SortEvent += program.OnSort;
+
+        Console.WriteLine("1 is for ascending order and 2 is for descending one:");
+        string input = Console.ReadLine();
+
+        try
+        {
+            int sortOrder = int.Parse(input);
+            program.SortEvent?.Invoke(sortOrder);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Incorrect. Please, insert 1 or 2.");
+        }
+
+        foreach (string surname in program.surnames)
+        {
+            Console.WriteLine(surname);
+        }
+    }
+
+    private void OnSort(int sortOrder)
+    {
+        if (sortOrder == 1)
+        {
+            surnames.Sort();
+        }
+        else if (sortOrder == 2)
+        {
+            surnames.Sort();
+            surnames.Reverse();
+        }
+        else
+        {
+            Console.WriteLine("Incorrect order.");
+        }
+    }
+}
+
